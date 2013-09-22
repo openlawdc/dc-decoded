179e97242146b9459b9225e218e0112e2794d2ed
Tom MacWright <tom@macwright.org>
2013-09-22 17:57:21 -0400

    Fix some bugs, add others

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,8 @@
 var Tokenizer = require('tokenizer'),
     fs = require('fs'),
-    t = new Tokenizer();
+    t = Tokenizer(function() {
+        // console.log(arguments);
+    });
 
 t.addRule(/^History$/, 'history');
 
@@ -10,20 +12,26 @@ t.addRule(/^\s*ARTICLE\s+(\w+)$/, 'article');
 t.addRule(/^\s*CHAPTER\s+(\d+)$/, 'chapter');
 t.addRule(/^\s*TITLE\s+(\d+)$/, 'title');
 
-// // The text of the law
-t.addRule(/^\w+$/, 'word');
-t.addRule(/^(\.|\,|\-|\;|\(|\)|\/|\:|\"|\')$/, 'punctuation');
-t.addRule(/^(\-?[\d]+)$/, 'number');
-t.addRule(/^(\s)+$/, 'whitespace');
+// text of the law
+t.addRule(/^([\r|\n]*)$/, 'space');
+t.addRule(/^(.*)$/, 'anything');
 
 module.exports = function parse(file) {
     var stream = fs.createReadStream(file, { encoding: 'utf8' });
 
     stream.pipe(t).on('token', handleToken).on('end', function() {
         console.log('done');
+    }).on('error', function() {
+        console.log(arguments);
     });
+    //
+    // stream.on('data', function(chunk) {
+    //     console.log(chunk.toString());
+    // });
 
     function handleToken(token, type) {
-        console.log(type);
+        if (type !== 'space' && type !== 'anything') {
+            console.log(token.toString());
+        }
     }
 };
